a1e1d2573ddc97e5e8eb6081be3043123493ec5f
lukkan95 <lukasz.szatan.95@gmail.com>
2023-08-16 16:55:28 +0200

    Added tests

--- tests/test_App.py
+++ tests/test_App.py
@@ -2,6 +2,7 @@ import unittest
 from datetime import datetime
 from App import Main
 import os
+import tkinter as tk
 
 
 class Test(unittest.TestCase):
@@ -24,6 +25,16 @@ class Test(unittest.TestCase):
             assert line[0] != 0
         path = f'{os.path.dirname(os.path.realpath(__file__))}\{temp_file}'
         os.remove(path)
+
+    def test_add_checkbutton_and_log_status_of_checkbutton(self):
+        temp_checkbutton = Main.Figure1().add_checkbutton('test_checkbutton', '0', progressbar=None, label_progressbar=None)
+        # temp_file = Main.DataLogs().create_text_file()
+        Main.DataLogs.log_status_of_checkbutton(listbox=None, name='Test', value=, sheet_name='0', filename=temp_file)
+
+
+        assert temp_checkbutton
+
+
 
 
 
